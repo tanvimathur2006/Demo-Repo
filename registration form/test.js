ed841f4e79a8fc8f805f007dcf61a097b14f0f81
tanvimathur2006 <tanvimathur.2006@gmail.com>
2026-08-05 11:24:18 +0530

    committing uncommitted files

--- registration form/test.js
+++ registration form/test.js
@@ -22,3 +22,35 @@ else{
 }
 
 // TC-03:Check script.js
+if (fs.existsSync("script.js")){
+    console.log("TC-03 : script.js exists :PASS");
+}
+else{
+    console.log("TC-03 : script.js exists :FAIL");
+    passed=false;
+}
+
+//tc4 :
+if(fs.existsSync("student.json")){
+    console.log("TC-04 : student.json exists :PASS");
+}
+else{
+    console.log("TC-04 : student.json exists :FAIL");
+    passed=false;
+}
+
+//read json 
+const student = JSON.parse(
+    fs.readFileSync("student.json")
+);
+
+
+// TC-05: Name Validation
+if (fullName !== "") {
+    console.log("TC-05 Passed: Name is not empty");
+} else {
+    console.log("TC-05 Failed: Name is empty");
+}
+
+// TC-06: 
+
